e190570a1c8367df3e4cb235eaad75ddcbd0ae7b
duanyuwen <duanyuwen@meituan.com>
2018-10-17 19:24:52 +0800

    fix install

--- install.js
+++ install.js
@@ -1,9 +1,5 @@
 var path = require("path");
 var copy = require("copy");
-var root = process.mainModule.paths
-  .filter(p => !p.includes("node_modules"))
-  .shift();
-console.log(root, process.mainModule.paths);
 
 var copyPath = "./src/utils";
 
@@ -11,8 +7,7 @@ init();
 
 function init() {
   parsePath();
-
-  copy("./src/*.js", path.join(root, copyPath), function(err, file) {
+  copy(__dirname + "/src/*.js", path.resolve(copyPath), function(err, file) {
     if (err) {
       console.log("copy, fail: ", err);
       return;
@@ -23,7 +18,7 @@ function init() {
 
 function parsePath() {
   try {
-    json = require(path.join(root, "package.json"));
+    json = require(path.resolve("package.json"));
     copyPath = json["guji-path"] || copyPath;
   } catch (e) {
     console.log("package.json fail, use default path: " + copyPath, e);
