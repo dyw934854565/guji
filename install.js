42228b918c7cba5fb1f32eefbd2f7396e5462e61
duanyuwen <duanyuwen@meituan.com>
2018-12-11 11:14:50 +0800

    rm install

--- install.js
+++ install.js
@@ -1,26 +1,26 @@
-var path = require("path");
-var copy = require("copy");
+// var path = require("path");
+// var copy = require("copy");
 
-var copyPath = "./src/utils";
+// var copyPath = "./src/utils";
 
-init();
+// init();
 
-function init() {
-  parsePath();
-  copy(__dirname + "/src/*.js", path.resolve(process.cwd(), copyPath), function(err, file) {
-    if (err) {
-      console.log("copy, fail: ", err);
-      return;
-    }
-    console.log("files coped: ", file.length);
-  });
-}
+// function init() {
+//   parsePath();
+//   copy(__dirname + "/src/*.js", path.resolve(process.cwd(), copyPath), function(err, file) {
+//     if (err) {
+//       console.log("copy, fail: ", err);
+//       return;
+//     }
+//     console.log("files coped: ", file.length);
+//   });
+// }
 
-function parsePath() {
-  try {
-    json = require(path.resolve("package.json"));
-    copyPath = json["guji-path"] || copyPath;
-  } catch (e) {
-    console.log("package.json fail, use default path: " + copyPath, e);
-  }
-}
+// function parsePath() {
+//   try {
+//     json = require(path.resolve("package.json"));
+//     copyPath = json["guji-path"] || copyPath;
+//   } catch (e) {
+//     console.log("package.json fail, use default path: " + copyPath, e);
+//   }
+// }
