914766b200c6ecb4534862503600e9bf38b822d9
duanyuwen <duanyuwen@meituan.com>
2019-06-12 11:36:08 +0800

    add build and update version

---
+++ dist/timeout.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = timeout;
+
+var _getDefer = require('./getDefer');
+
+var _getDefer2 = _interopRequireDefault(_getDefer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function timeout(fn, ms = 100, _this) {
+  return function (...args) {
+    const defer = (0, _getDefer2.default)(ms);
+    return Promise.race([fn.apply(_this || this, args), defer.promise]);
+  };
+}
