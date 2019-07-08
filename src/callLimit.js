1e866303a658c52ec8492eb9d8ceb7b146448c62
duanyuwen <duanyuwen@meituan.com>
2019-07-08 12:25:20 +0800

    fix calLimit

--- src/callLimit.js
+++ src/callLimit.js
@@ -1,8 +1,8 @@
 export default function callLimit(fn, limit = 1, _this, callback = console.log) {
-  let counter = 1
+  let counter = 0
   return function (...args) {
+    counter++
     if (counter <= limit) {
-      counter++
       return fn.apply(_this || this, args);
     }
     callback && callback(counter, 'extra call');
