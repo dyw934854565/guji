c15a4107cb65401ae150780c585d327ea956aaf7
duanyuwen <duanyuwen@meituan.com>
2019-07-11 13:59:59 +0800

    fix d.ts

--- src/timer.d.ts
+++ src/timer.d.ts
@@ -1,7 +1,7 @@
-interface timer {
+interface Timer {
   start: (str: string) => void,
   end: (str: string, fun: (str: string, timer: number) => void) => void
 }
-export declare function create (): timer
-
+export declare function create(): Timer
+declare const timer: Timer;
 export default timer
