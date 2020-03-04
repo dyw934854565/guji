5faab24ad139af11e55294203ffaa3bb4a71dae1
crystalmeng <crystalmeng@tencent.com>
2020-03-04 10:07:50 +0800

    run build

--- dist/safeParse.js
+++ dist/safeParse.js
@@ -4,13 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = safeParse;
-function safeParse(str, defaultVal) {
+function safeParse(str, defaultVal, onerror = console.log) {
   if (typeof str !== 'string') {
     return str;
   }
   try {
     return JSON.parse(str);
   } catch (e) {
+    onerror(e, str, 'json parse error');
     return typeof defaultVal === 'undefined' ? str : defaultVal;
   }
 }
